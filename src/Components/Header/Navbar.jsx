95d33e8894e22589f4c9b388d695080ea5da83b5
Panuwat <netpopo1150@outlook.com>
2024-02-21 14:56:35 +0700

    make toggle menu

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menuToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className=" w-[90vw]  md:max-w-[1100px] h-28 border  m-auto flex justify-between items-center">
+    <div className=" w-[90vw]  md:max-w-[1100px] h-28  m-auto flex justify-between items-center relative">
       <div>
         <img src="/image/Logo.svg" alt="Logo" />
       </div>
+
       <div className="hidden md:flex justify-center items-center ">
         <ul className="flex space-x-5 mx-6 lg:space-x-9 lg:mx-8 text-[#090f4e]  text-base lg:text-lg transition-all ease-in-out duration-300">
           <li>
@@ -33,6 +43,50 @@ function Navbar() {
         >
           Try Free
         </a>
+      </div>
+
+      <div onClick={menuToggle} className="cursor-pointer md:hidden">
+        {isOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+      </div>
+
+      <div
+        className={
+          !isOpen
+            ? "absolute top-[-180px] flex-col w-full opacity-0 transition-all ease-in-out duration-300 text-center"
+            : "absolute top-[110px]  w-full h-full transition-all ease-in-out duration-300 text-center"
+        }
+      >
+          <ul className="flex-col text-[#090f4e] text-lg space-y-5 mb-5">
+            <li>
+              <a
+                href="#"
+                className="hover:opacity-55 ease-in-out duration-100 "
+              >
+                Company
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:opacity-55 ease-in-out duration-100">
+                Feature
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:opacity-55 ease-in-out duration-100">
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:opacity-55 ease-in-out duration-100">
+                Log In
+              </a>
+            </li>
+          </ul>
+          <a
+            href="#"
+            className="ease-in-out duration-300 text-lg font-medium bg-[rgb(41,63,204)] hover:bg-opacity-70 px-12 py-3 text-white  rounded-[6px]"
+          >
+            Try Free
+          </a>
       </div>
     </div>
   );
