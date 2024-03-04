fb186706982f71dc92dd1350fd09a6c0638cfa95
Panuwat <netpopo1150@outlook.com>
2024-03-04 23:02:21 +0700

    make component and responsive newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import PerfectSolutions from "./Components/PerfectSolutions/PerfectSolutions";
 import Discover from "./Components/Discover/Discover";
 import Testimonial from "./Components/Testimonial/Testimonial";
 import Subscription from "./Components/Subscription/Subscription";
+import SignUp from "./Components/SignUp/SignUp";
 function App() {
   return (
     <div>
-      
       <Header />
       <LogoCompany />
       <PerfectSolutions />
       <Discover />
       <Testimonial />
       <Subscription />
+      <SignUp />
     </div>
   );
 }
